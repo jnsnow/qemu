f66e9ced1db1f26410244dbf862c37cbacf8fceb
John Snow <jsnow@redhat.com>
2016-10-04 13:41:09 -0400

    blockjob: add .clean property
    Cleaning up after we have deferred to the main thread but before the
    transaction has converged can be dangerous and result in deadlocks
    if the job cleanup invokes any BH polling loops.
    
    A job may attempt to begin cleaning up, but may induce another job to
    enter its cleanup routine. The second job, part of our same transaction,
    will block waiting for the first job to finish, so neither job may now
    make progress.
    
    To rectify this, allow jobs to register a cleanup operation that will
    always run regardless of if the job was in a transaction or not, and
    if the transaction job group completed successfully or not.
    
    Move sensitive cleanup to this callback instead which is guaranteed to
    be run only after the transaction has converged, which removes sensitive
    timing constraints from said cleanup.
    
    Furthermore, in future patches these cleanup operations will be performed
    regardless of whether or not we actually started the job. Therefore,
    cleanup callbacks should essentially confine themselves to undoing create
    operations, e.g. setup actions taken in what is now backup_start.
    
    Reported-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>

--- block/backup.c
+++ block/backup.c
@@ -242,6 +242,13 @@ static void backup_abort(BlockJob *job)
     }
 }
 
+static void backup_clean(BlockJob *job)
+{
+    BackupBlockJob *s = container_of(job, BackupBlockJob, common);
+    assert(s->target);
+    blk_unref(s->target);
+}
+
 static void backup_attached_aio_context(BlockJob *job, AioContext *aio_context)
 {
     BackupBlockJob *s = container_of(job, BackupBlockJob, common);
@@ -306,6 +313,7 @@ static const BlockJobDriver backup_job_driver = {
     .set_speed              = backup_set_speed,
     .commit                 = backup_commit,
     .abort                  = backup_abort,
+    .clean                  = backup_clean,
     .attached_aio_context   = backup_attached_aio_context,
 };
 
@@ -327,10 +335,7 @@ typedef struct {
 
 static void backup_complete(BlockJob *job, void *opaque)
 {
-    BackupBlockJob *s = container_of(job, BackupBlockJob, common);
     BackupCompleteData *data = opaque;
-
-    blk_unref(s->target);
 
     block_job_completed(job, data->ret);
     g_free(data);
@@ -642,7 +647,7 @@ void backup_start(const char *job_id, BlockDriverState *bs,
         bdrv_reclaim_dirty_bitmap(bs, sync_bitmap, NULL);
     }
     if (job) {
-        blk_unref(job->target);
+        backup_clean(&job->common);
         block_job_unref(&job->common);
     }
 }
