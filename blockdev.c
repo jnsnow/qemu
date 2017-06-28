19a6311ed09823ff2fb2b2bde4932cf53707c1c9
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2017-06-28 15:05:10 +0300

    block/dirty-bitmap: add readonly field to BdrvDirtyBitmap
    It will be needed in following commits for persistent bitmaps.
    If bitmap is loaded from read-only storage (and we can't mark it
    "in use" in this storage) corresponding BdrvDirtyBitmap should be
    read-only.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-id: 20170628120530.31251-11-vsementsov@virtuozzo.com

--- blockdev.c
+++ blockdev.c
@@ -2023,6 +2023,9 @@ static void block_dirty_bitmap_clear_prepare(BlkActionState *common,
     } else if (!bdrv_dirty_bitmap_enabled(state->bitmap)) {
         error_setg(errp, "Cannot clear a disabled bitmap");
         return;
+    } else if (bdrv_dirty_bitmap_readonly(state->bitmap)) {
+        error_setg(errp, "Cannot clear a readonly bitmap");
+        return;
     }
 
     bdrv_clear_dirty_bitmap(state->bitmap, &state->backup);
@@ -2790,6 +2793,9 @@ void qmp_block_dirty_bitmap_clear(const char *node, const char *name,
         error_setg(errp,
                    "Bitmap '%s' is currently disabled and cannot be cleared",
                    name);
+        return;
+    } else if (bdrv_dirty_bitmap_readonly(bitmap)) {
+        error_setg(errp, "Bitmap '%s' is readonly and cannot be cleared", name);
         return;
     }
 
