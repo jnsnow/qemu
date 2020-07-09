069da9cfb4d42c2a2907e60edd00359b4f10e23d
John Snow <jsnow@redhat.com>
2020-07-09 18:12:00 -0400

    python/machine.py: Close QMP socket in cleanup
    It's not important to do this before waiting for the process to exit, so
    it can be done during generic post-shutdown cleanup.

--- python/qemu/machine.py
+++ python/qemu/machine.py
@@ -283,6 +283,10 @@ class QEMUMachine:
             self._qmp.accept()
 
     def _post_shutdown(self):
+        if self._qmp:
+            self._qmp.close()
+            self._qmp = None
+
         self._load_io_log()
 
         if self._qemu_log_file is not None:
@@ -355,8 +359,6 @@ class QEMUMachine:
         Wait for the VM to power off
         """
         self._popen.wait()
-        if self._qmp:
-            self._qmp.close()
         self._post_shutdown()
 
     def shutdown(self, has_quit=False, hard=False):
@@ -377,7 +379,6 @@ class QEMUMachine:
                 try:
                     if not has_quit:
                         self._qmp.cmd('quit')
-                    self._qmp.close()
                     self._popen.wait(timeout=3)
                 except:
                     self._popen.kill()
